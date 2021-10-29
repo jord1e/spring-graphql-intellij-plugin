55aebd24de1ee7467c1d72894b009f994661edfc
GitHub Action <action@github.com>
2021-10-29 20:29:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/jord1e/intellijspringgraphqlplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jord1e/intellijspringgraphqlplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jord1e.intellijspringgraphqlplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jord1e.intellijspringgraphqlplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/jord1e/intellijspringgraphqlplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jord1e.intellijspringgraphqlplugin.services
+
+import com.github.jord1e.intellijspringgraphqlplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
